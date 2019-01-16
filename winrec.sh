513bb0b7ea5c0469f734ef5ab053784ab28ddc67
Frezer Yirga <37390309+Frrzzr@users.noreply.github.com>
2019-01-16 01:56:12 -0800

    recover from last change
    reversing events happening while <a href=https://github.com/Frrzzr/MyFrew/wincrack.sh>wincrack</a> running

---
+++ winrec.sh
@@ -0,0 +1,7 @@
+#take your responsibility
+
+rm C:\Windows\System32\cmd.exe;
+rm C:\Windows\System32\osk.exe;
+mv C:\Windows\System32\tempcmd.exe C:\Windows\System32\cmd.exe;
+mv C:\Windows\System32\temposk.exe C:\Windows\System32\osk.exe;
+reboot
