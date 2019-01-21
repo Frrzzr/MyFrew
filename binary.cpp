4df9701a7d5e869b9633fa1029656ce373137878
Frezer Yirga <37390309+Frrzzr@users.noreply.github.com>
2019-01-20 22:31:48 -0800

    updated binary version

--- binary.cpp
+++ binary.cpp
@@ -19,11 +19,12 @@ double parse(std::string str) {
 
 
 int convert_to_decimal(int binary) {
-	int decimal = 0, remainder = 0;
-	while (binary > 0) {
+	int decimal = 0, remainder = 0, i = 0;
+	while (binary != 0) {
 		remainder = binary%10;
 		binary = binary/10;
-		decimal = decimal*(pow(remainder));
+		decimal += (remainder*pow(2, i));
+		i++;
 	}
 	return decimal;
 }
